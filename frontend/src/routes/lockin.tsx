b1af940fccb407a9d9d07f49da6cade37b106731
sailalith2005 <140128409+sailalithkanumuri@users.noreply.github.com>
2025-01-15 00:23:11 -0500

    wip
    wip

--- frontend/src/routes/lockin.tsx
+++ frontend/src/routes/lockin.tsx
@@ -4,6 +4,7 @@ import { gameStateStore } from "../state";
 import { useEffect, useRef } from "react";
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import { isLookingAwayStore } from "../mediapipe";
+import "@tensorflow/tfjs-backend-webgl";
 
 export const Route = createFileRoute("/lockin")({
   component: LockInComponent,
@@ -32,17 +33,20 @@ function LockInComponent() {
 }
 
 function WebcamView({ stream }: { stream: MediaStream }) {
-  
   // Define references
   const videoRef = useRef<HTMLVideoElement>(null);
 
   // Detect function
-  const detect = async (net:any) => {
-    if (typeof videoRef.current !== "undefined" && videoRef.current !== null && videoRef.current.readyState === 4) {
-
-      const face = await net.estimateFaces({input:videoRef});
+  const detect = async (net: any) => {
+    if (
+      typeof videoRef.current !== "undefined" &&
+      videoRef.current !== null &&
+      videoRef.current.readyState === 4
+    ) {
+      const face = await net.estimateFaces(videoRef.current);
+      console.log(face);
       const landmarks: any[] = face.scaledMesh;
-      
+
       let noseTip: any, leftNose: any, rightNose: any;
       try {
         noseTip = { ...landmarks[1], name: "nose tip" };
@@ -58,15 +62,16 @@ function WebcamView({ stream }: { stream: MediaStream }) {
         z: (leftNose!.z + rightNose!.z) / 2,
       };
 
-      const zaxis = { x: noseTip.x, y: midpoint.y, z:  noseTip.z };
-      const xaxis = { x: midpoint.x+1, y: midpoint.y, z: midpoint.z };
-    
+      const zaxis = { x: noseTip.x, y: midpoint.y, z: noseTip.z };
+      const xaxis = { x: midpoint.x + 1, y: midpoint.y, z: midpoint.z };
+
       const yaw = getAngleBetweenLines(midpoint, noseTip, xaxis);
       const pitch = getAngleBetweenLines(midpoint, noseTip, zaxis);
 
       console.log([yaw, pitch]);
 
-      if (Math.abs(yaw) < 30 || Math.abs(yaw) > 150 || Math.abs(pitch) > 60) { // not looking at screen
+      if (Math.abs(yaw) < 30 || Math.abs(yaw) > 150 || Math.abs(pitch) > 60) {
+        // not looking at screen
         console.log("looking away");
         isLookingAwayStore.getState().lookAway();
       } else {
@@ -74,26 +79,26 @@ function WebcamView({ stream }: { stream: MediaStream }) {
         isLookingAwayStore.getState().lookAtScreen();
       }
     }
-  }
+  };
 
-  function getAngleBetweenLines(midpoint:any, point1:any, point2:any) {
+  function getAngleBetweenLines(midpoint: any, point1: any, point2: any) {
     const v1 = { x: point1.x - midpoint.x, y: point1.y - midpoint.y };
     const v2 = { x: point2.x - midpoint.x, y: point2.y - midpoint.y };
-  
+
     const dotProduct = v1.x * v2.x + v1.y * v2.y;
-  
+
     const len1 = Math.sqrt(v1.x * v1.x + v1.y * v1.y);
     const len2 = Math.sqrt(v2.x * v2.x + v2.y * v2.y);
-  
+
     // Calculate the cosine of the angle between the two vectors
     const cosAng = dotProduct / (len1 * len2);
-  
+
     // Use the arccosine function to get the angle in radians
     const angRad = Math.acos(cosAng);
-  
+
     // Convert the angle to degrees
     const angDegrees = (angRad * 180) / Math.PI;
-  
+
     return angDegrees;
   }
 
@@ -117,15 +122,21 @@ function WebcamView({ stream }: { stream: MediaStream }) {
     };
   }, []);
 
-  // Load facemesh
-  const runFacemesh = async () => {
-    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
-    setInterval(() => {
-      detect(net);
-    }, 3000);
-  };
 
-  useEffect(()=>{runFacemesh()}, []);
+  useEffect(() => {
+    const interval = (async () => {
+      const net = await facemesh.createDetector(
+        facemesh.SupportedModels.MediaPipeFaceMesh,
+        {
+          runtime: "tfjs",
+          refineLandmarks: false
+        }
+      );
+      return setInterval(() => detect(net), 1000);
+    })()
+
+    return () => {interval.then((interval) => clearInterval(interval))}
+  }, []);
 
   return (
     <video
